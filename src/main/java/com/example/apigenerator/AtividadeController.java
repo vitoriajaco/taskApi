cc2927b6dc1a9b79772a51c903f11930a68fa139
vitoriajaco <vitoriajaco@gmail.com>
2024-02-21 18:45:13 -0300

    Alteraçoes de http

--- src/main/java/com/example/apigenerator/AtividadeController.java
+++ src/main/java/com/example/apigenerator/AtividadeController.java
@@ -1,13 +1,15 @@
 package com.example.apigenerator;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
+
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Optional;
 
-@Controller
+@RestController
+@RequestMapping("/api/task")
 public class AtividadeController {
 
     @Autowired
@@ -25,6 +27,7 @@ public class AtividadeController {
     }
 
     @PostMapping(path= "/cadastrarAtividade")
+    @ResponseStatus(HttpStatus.CREATED)
     public Atividade cadastrarAtividade (@RequestBody Atividade atividade){
         return atividadeService.cadastrarAtividade(atividade);
     }
