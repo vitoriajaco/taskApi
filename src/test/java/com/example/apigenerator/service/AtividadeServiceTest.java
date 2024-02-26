688398d614d5ea358ff9b2df0623d7ec71eb3efd
vitoriajaco <vitoriajaco@gmail.com>
2024-02-26 17:33:40 -0300

    Teste positivo do método mostrarTodasAsAtividades

--- src/test/java/com/example/apigenerator/service/AtividadeServiceTest.java
+++ src/test/java/com/example/apigenerator/service/AtividadeServiceTest.java
@@ -17,8 +17,11 @@ import org.mockito.internal.matchers.NotNull;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.mock.mockito.MockBean;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 
+import static org.mockito.ArgumentMatchers.anyList;
 import static org.mockito.ArgumentMatchers.anyLong;
 
 class AtividadeServiceTest {
@@ -58,6 +61,20 @@ class AtividadeServiceTest {
 
     @Test
     void mostrarTodasAtividades() {
+        List<Atividade>listaDeAtividades = new ArrayList<>();
+        Atividade atividade2 = new Atividade(2L, "Correr uma maratona", Status.EM_ABERTO, Categoria.PESSOAL);
+        Atividade atividade3 = new Atividade(3L, "Comprar batata doce", Status.CONCLUIDA, Categoria.MERCADO);
+        listaDeAtividades.add(atividade);
+        listaDeAtividades.add(atividade2);
+        listaDeAtividades.add(atividade3);
+
+        Mockito.when(atividadeRepository.findAll()).thenReturn(listaDeAtividades);
+
+        List<Atividade> result = atividadeService.mostrarTodasAtividades();
+
+        Assertions.assertEquals(result, listaDeAtividades);
+        System.out.println(result);
+        System.out.println(listaDeAtividades);
     }
 
     @Test
