22bbb97ed36a4d5412c94d023be331396259d4fb
vitoriajaco <vitoriajaco@gmail.com>
2024-02-26 14:32:17 -0300

    Estrutura inicial de teste

---
+++ src/test/java/com/example/apigenerator/repository/AtividadeRepositoryTest.java
@@ -0,0 +1,12 @@
+package com.example.apigenerator.repository;
+
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.ActiveProfiles;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@DataJpaTest
+@ActiveProfiles("test")
+class AtividadeRepositoryTest {
+
+}
---
+++ src/test/java/com/example/apigenerator/service/AtividadeServiceTest.java
@@ -0,0 +1,32 @@
+package com.example.apigenerator.service;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AtividadeServiceTest {
+
+    @Test
+    void mostrarTodasAtividades() {
+    }
+
+    @Test
+    void buscarAtividadePorId() {
+    }
+
+    @Test
+    void cadastrarAtividade() {
+    }
+
+    @Test
+    void alterarAtividade() {
+    }
+
+    @Test
+    void update() {
+    }
+
+    @Test
+    void deletarAtividade() {
+    }
+}
