6360e4494544da7415f6e9e489d15387e7e515fa
vitoriajaco <vitoriajaco@gmail.com>
2024-02-29 14:59:11 -0300

    Finalizando testes de controller

--- src/test/java/com/example/apigenerator/controller/AtividadeControllerTest.java
+++ src/test/java/com/example/apigenerator/controller/AtividadeControllerTest.java
@@ -3,13 +3,16 @@ package com.example.apigenerator.controller;
 import com.example.apigenerator.Enum.Categoria;
 import com.example.apigenerator.Enum.Status;
 import com.example.apigenerator.model.Atividade;
+import com.example.apigenerator.repository.AtividadeRepository;
 import com.example.apigenerator.service.AtividadeService;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -34,10 +37,17 @@ class AtividadeControllerTest {
 
     private Atividade atividade;
 
+    @Mock
+    private ResponseEntity<Atividade> responseEntityAtividade;
+
     @InjectMocks
     private AtividadeController atividadeController;
     @Mock
     private AtividadeService atividadeService;
+    @Autowired
+    @Mock
+    private AtividadeRepository atividadeRepository;
+
 
     @BeforeEach
     void setUp(){
@@ -46,7 +56,7 @@ class AtividadeControllerTest {
     }
 
     @Test
-    void DeveMostrarStatus200AoListarTodasAtividades() {
+    void deveMostrarStatus200AoListarTodasAtividades() {
         Mockito.when(atividadeService.mostrarTodasAtividades()).thenReturn(List.of(atividade));
         ResponseEntity<List<Atividade>> result = atividadeController.mostrarTodasAtividades();
 
@@ -56,7 +66,7 @@ class AtividadeControllerTest {
     }
 
     @Test
-    void AobuscarAtividadePorIdDeveRetornarStatusOk() {
+    void aoBuscarAtividadePorIdDeveRetornarStatusOk() {
         Mockito.when(atividadeService.buscarAtividadePorId(anyLong())).thenReturn(Optional.of(atividade));
         ResponseEntity<Atividade>result = atividadeController.alterarAtividade(ID, atividade);
 
@@ -67,14 +77,14 @@ class AtividadeControllerTest {
     }
 
     @Test
-    void AocadastrarAtividadeDeveRetornarStatusOk() {
+    void aoCadastrarAtividadeDeveRetornarStatusOk() {
         Mockito.when(atividadeService.cadastrarAtividade(any())).thenReturn(atividade);
         ResponseEntity<Atividade> result = atividadeController.cadastrarAtividade(atividade);
         assertEquals(HttpStatus.OK, result.getStatusCode());
     }
 
     @Test
-    void AoalterarAtividadeDeveRetornarStatusCodeOk() {
+    void aoAlterarAtividadeDeveRetornarStatusCodeOk() {
         Mockito.when(atividadeService.alterarAtividade(any(), anyLong())).thenReturn(atividade);
         ResponseEntity<Atividade> result = atividadeController.alterarAtividade(ID, atividade);
 
@@ -84,11 +94,30 @@ class AtividadeControllerTest {
     }
 
     @Test
-    void updateTask() {
+    void aoEditarAtividadeDeveRetornar200OKMetodoUpdate() {
+      atividade.getId();
+      atividade.setTarefa("Aprender Cobol");
+      atividade.getStatus();
+      atividade.getCategoria();
+
+
+
+      Mockito.when(atividadeService.update(any(), anyLong())).thenReturn(responseEntityAtividade);
+      Mockito.when(atividadeRepository.findById(anyLong())).thenReturn(Optional.of(atividade));
+      Mockito.when(atividadeRepository.save(atividade)).thenReturn(atividade);
+
+
+      ResponseEntity<Atividade> result = atividadeController.updateTask(ID, atividade);
+
+
+      Assertions.assertEquals(HttpStatus.OK, result.getStatusCode());
+      assertNotNull(result);
+
+
     }
 
     @Test
-    void deletarAtividade() {
+    void deletarAtividadeERetornarStatusOK() {
         Mockito.doNothing().when(atividadeService).deletarAtividade(anyLong());
 
         ResponseEntity<?> result = atividadeController.deletarAtividade(ID);
@@ -99,7 +128,7 @@ class AtividadeControllerTest {
     }
 
     @Test
-    void buscarPorId() {
+    void buscarPorIdRetornandoStatusOK() {
         Mockito.when(atividadeService.buscarAtividadePorId(anyLong())).thenReturn(Optional.of(atividade));
         ResponseEntity<Optional<Atividade>> result = atividadeController.buscarAtividadePorId(ID);
 
